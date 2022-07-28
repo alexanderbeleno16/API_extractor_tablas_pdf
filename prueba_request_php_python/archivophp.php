2c1e938c95f0b3d1b0d2e57df73c2f60980a5442
alexanderbeleno16 <alexanderbeleno16@gmail.com>
2022-07-28 17:40:50 -0500

    Paso #2: Pendiente

--- prueba_request_php_python/archivophp.php
+++
@@ -1,8 +0,0 @@
-<?php
-ob_start();
-        
-system("python3 " . __DIR__ ."/../Py/file.py parametro_prueba");
-
-$result = ob_get_contents();
-
-ob_end_clean();
--- prueba_request_php_python/archivopython.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-# sys.argv[1]
-parameter = sys.argv[1]
-print(parameter)
